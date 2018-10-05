75a8145bf6600854851048f3acaa8e68b4b6a984
Albert Chai <albertchai@chaisystems.net>
2018-10-04 18:13:24 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# graphical-network-tool
+# Graphical Network Tool Scripts
+Originally Social Network Analysis Generation 2 Scripts
+## Introduction
+From recent developments, we realized that we need to imrpove the functionality of our scripts in order to better show our results from our data inputs.  
+With the new generation 2 scripts, we have made much more improvements.  
+
+## Change Log
+Change log can be viewed at the CHANGELOG file  
+
+## Authors
+Albert Chai and Andrew S Lee  
+Based on the original scripts by Albert Chai, Joshua P. Le, and Andrew S. Lee.  
+Principal Investigator: Stanley M. Lo  
+MIT License  
+Made by the cats of the UCSD Lo Lab Group  
---
+++ start.R
@@ -0,0 +1,42 @@
+# Script Loader
+# UCSD Lo Lab Group Social Network Analysis Script
+# MIT License
+# Written by Albert Chai, Andrew S. Lee and Joshua P. Le
+
+# Welcome Message
+cat('UCSD Lo Lab Group Social Network Analysis Script\n')
+cat('MIT License\n')
+cat('Written by Albert Chai, Andrew S. Lee, and Joshua P. Le\n')
+cat('\n')
+cat('Please read the man pages on GitHub for detailed instructions\n')
+invisible(readline(prompt = "Press [enter] to continue\n"))
+
+# Check User Directory
+cat('Your current working directory is: \n')
+print(getwd()) #Gets users current working directory
+checkwdloc <- readline('Please confirm that this is the correct directory [Y/n]: ')
+if(checkwdloc == "n" || checkwdloc == "N"){
+  # Ask user to define the current working directory of project files
+  getcorrdir <- readline('Enter the directory of where the script files are located (no quotes): ')
+  setwd(getcorrdir) # Sets working directory based on user input
+}
+
+# Run directory verification script
+if(checkwdloc == "" || checkwdloc == "Y" || checkwdloc == "y"){
+  source('setdircheck.R', echo = FALSE)
+}
+
+# Remainder of script only runs if the Set Directory Check Script has ran, based on variable set
+if(dirsettrue ==1){
+  # Checks Workspace Environment Variables
+  checkwksp <- readline('Are you using data from a previous script session? [y/N]: ')
+  if(checkwksp == "n" || checkwksp == "N" || checkwksp == ""){
+    # Clear environmental variables
+    rm(list=ls())
+    # Initialize Loader Variables
+    source('dependencies/varloader.R', echo = FALSE)
+  }
+  
+  # Loads external script to check for package dependencies
+  source('dependencies/libraries.R', echo = FALSE)
+}
